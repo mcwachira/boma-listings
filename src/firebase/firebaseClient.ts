5e3345e32a866e703e55886064315df5c2d92aba
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-04-21 21:47:47 +0300

    initialized the app  with nextjs and setup firebase client for auth and storage and firebase server for firestore

---
+++ src/firebase/firebaseClient.ts
@@ -0,0 +1,35 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApps } from "firebase/app";
+import {FirebaseStorage, getStorage} from "@firebase/storage";
+import {Auth, getAuth} from "@firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBtne4VdBjDOZjRwORVOovxasbh4iQ6OLA",
+    authDomain: "boma-listing.firebaseapp.com",
+    projectId: "boma-listing",
+    storageBucket: "boma-listing.firebasestorage.app",
+    messagingSenderId: "1517797817",
+    appId: "1:1517797817:web:08cbcc381000eed3207668"
+};
+
+// Initialize Firebase
+const currentApps = getApps()
+
+let auth:Auth;
+let storage:FirebaseStorage
+
+//checks if an app has  been initialized and if not initialize the app
+if(!currentApps.length){
+    const app = initializeApp(firebaseConfig);
+    auth = getAuth(app);
+    storage = getStorage(app);
+}else{
+    const app = currentApps[0];
+    auth = getAuth(app);
+    storage = getStorage(app);
+}
+
+export {auth , storage}
---
+++ src/firebase/firebaseServer.ts
@@ -0,0 +1,36 @@
+import {getApps, ServiceAccount} from "firebase-admin/app";
+import  admin,{initializeApp} from "firebase-admin";
+import {getFirestore, Firestore} from "firebase-admin/firestore";
+
+
+const serviceAccount ={
+    "type": "service_account",
+    "project_id": "boma-listing",
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "client_email":process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id":process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40boma-listing.iam.gserviceaccount.com",
+    "universe_domain": "googleapis.com"
+}
+
+
+let firestore:Firestore;
+const currentApps = getApps()
+
+
+//checks if an app has  been initialized and if not initialize the app
+if(!currentApps.length){
+    const app = initializeApp({
+        credential:admin.credential.cert(serviceAccount as ServiceAccount),
+    });
+   firestore = getFirestore(app)
+}else{
+    const app = currentApps[0];
+   firestore = getFirestore(app)
+}
+
+export {firestore}
