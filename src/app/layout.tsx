f6be65c2096f3dcfed843ced7420fe07a2237638
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-04-23 22:36:26 +0300

    Setup auth with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Providers from "./providers";
+import {ClerkProvider} from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,6 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+      <ClerkProvider>
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
@@ -41,5 +43,6 @@ export default function RootLayout({
       </body>
 
     </html>
+      </ClerkProvider>
   );
 }
