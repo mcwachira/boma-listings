bcdb5225ebb07439311996e0b638f7468dfcb6e4
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-04-23 19:15:08 +0300

    updated my css theme and added dark mode to the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
+import Providers from "./providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,12 +29,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <Providers>
+          <Navbar/>
       <main className="container py-10">
 
 
-      <Navbar/>
+
         {children}
       </main>
+      </Providers>
       </body>
 
     </html>
