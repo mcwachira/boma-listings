53a21c9c23b61f9bcbcf48f06cbca556fc474e43
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-04-23 18:05:18 +0300

    reinitialized the app again

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,16 @@
 import type { Metadata } from "next";
-import { Poppins } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import {AuthProvider} from "@/context/auth";
 
-
-
-const poppins = Poppins({
-    subsets: ["latin"],
-    variable: "--font-poppins",
-    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,13 +25,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <AuthProvider>
-          <Navbar/>
-          {children}
-      </AuthProvider>
-
+        {children}
       </body>
     </html>
   );
