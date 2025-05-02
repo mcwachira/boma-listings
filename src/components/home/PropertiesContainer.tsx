00f71e4f7e08f8de2fda6cb2a8b2ba4102878e5b
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-02 19:15:04 +0300

    Updated PropertiesContainer.tsx and PropertiesList.tsx Components

--- src/components/home/PropertiesContainer.tsx
+++ src/components/home/PropertiesContainer.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import {PropertyCardProps} from "@/utils/types";
-import {fetchProperties} from "@/actions/action";
-import EmptyList from './EmptyList';
 import PropertiesList from './PropertiesList';
+import EmptyList from './EmptyList';
+import type {PropertyCardProps} from '@/utils/types';
+import {fetchProperties} from '@/actions/action';
 
-async function PropertiesContainer({category ,search}:{category?:string, search?:string}) {
-
-    const properties:PropertyCardProps[] = await fetchProperties({category, search});
-
-    if(properties.length === 0) {
-        return(
-            <EmptyList heading="No results" message="Try Changing or Removing some of your Filters" btnText="Clear filters" />
-        )
+async function PropertiesContainer({
+                                       category,
+                                       search,
+                                   }: {
+    category?: string;
+    search?: string;
+}) {
+    const properties: PropertyCardProps[] = await fetchProperties({
+        category,
+        search,
+    });
+    if (properties.length === 0) {
+        return (
+            <EmptyList
+                heading='No results.'
+                message='Try changing or removing some of your filters.'
+                btnText='Clear Filters'
+            />
+        );
     }
-    return (
-  <PropertiesList properties={properties}/>
-    );
-}
 
+    return <PropertiesList properties={properties} />;
+}
 export default PropertiesContainer;
