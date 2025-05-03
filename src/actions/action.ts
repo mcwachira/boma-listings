49ce018bdeeebda1dec752c04ae921d6dbba2796
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-03 14:25:08 +0300

    Created a server action to fetch favorites properties

--- src/actions/action.ts
+++ src/actions/action.ts
@@ -253,3 +253,31 @@ export const toggleFavoriteAction = async(prevState:{
         return renderError(error)
     }
 }
+
+export const fetchFavorites = async() => {
+    const user = await getAuthUser();
+
+   const favorites =  await prisma.favorite.findMany({
+        where:{
+            profileId:user.id
+        },
+        select: {
+            property: {
+                select:{
+                id: true,
+                name: true,
+                tagline: true,
+                country: true,
+                price: true,
+                image: true,
+
+                }
+            }
+        },
+        orderBy: {
+            createdAt: 'desc',
+        },
+    })
+
+    return favorites.map((favorite)=>favorite.property)
+}
