87de689760d56ee57896338441f9698d6c15fbfb
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-06 00:08:11 +0300

    Wrapped my NavSearch with as Suspense

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import React, {Suspense} from 'react';
 import Logo from "@/components/navbar/Logo";
 import NavSearch from "@/components/navbar/NavSearch";
 import DarkMode from "@/components/navbar/DarkMode";
@@ -5,16 +8,18 @@ import LinksDropDown from "@/components/navbar/LinksDropDown";
 
 function Navbar() {
     return (
-      <nav className="border-b">
-          <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
-
-              <Logo/>
-              <NavSearch/>
-              <div className="flex gap-4 items-center">
-                  <DarkMode/>
-                  <LinksDropDown/>
-              </div>          </div>
-      </nav>
+        <nav className="border-b">
+            <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
+                <Logo />
+                <Suspense fallback={null}>
+                    <NavSearch />
+                </Suspense>
+                <div className="flex gap-4 items-center">
+                    <DarkMode />
+                    <LinksDropDown />
+                </div>
+            </div>
+        </nav>
     );
 }
 
