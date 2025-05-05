a29a443699445dfbbb8d428b3956240ccd760799
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-06 00:13:52 +0300

    Created 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+
+export default function NotFound() {
+    return (
+
+        <div>
+        <h1>Not Found</h1>
+        <p>The requested resource could not be found.</p>
+        </div>
+
+    );
+}
