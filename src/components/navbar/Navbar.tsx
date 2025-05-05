3d7bbc86b42fb4141f8ed8b8953f25cf27bb745f
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-06 00:25:44 +0300

    updated issues causing site not to build

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,21 @@
-'use client';
-
-import React, {Suspense} from 'react';
 import Logo from "@/components/navbar/Logo";
-import NavSearch from "@/components/navbar/NavSearch";
 import DarkMode from "@/components/navbar/DarkMode";
 import LinksDropDown from "@/components/navbar/LinksDropDown";
 
+import dynamic from 'next/dynamic';
+
+// Dynamically import NavSearch so it's not included in the SSR build
+const NavSearch = dynamic(() => import('@/components/navbar/NavSearch'), {
+    ssr: false,
+});
 function Navbar() {
     return (
         <nav className="border-b">
             <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap gap-4 py-8">
                 <Logo />
-                <Suspense fallback={null}>
+
                     <NavSearch />
-                </Suspense>
+
                 <div className="flex gap-4 items-center">
                     <DarkMode />
                     <LinksDropDown />
