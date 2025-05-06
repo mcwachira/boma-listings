c8510cc0e33c157ac1999c111e1020002c12f766
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-06 21:21:06 +0300

    Updated TextAreaInput Component to enable my  create reveiw action to work

--- src/components/form/TextAreaInput.tsx
+++ src/components/form/TextAreaInput.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Label} from "@/components/ui/label";
-import { Textarea } from '@/components/ui/textarea';
+import {Textarea} from '@/components/ui/textarea';
 
 
 type TextAreaInputProps = {
