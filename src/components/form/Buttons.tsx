2f83cbb1f6147b25b385e137a286444e344edc80
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-07 00:33:13 +0300

    Added a delete button , finished the Review Page and created a loading.tsx for the review page

--- src/components/form/Buttons.tsx
+++ src/components/form/Buttons.tsx
@@ -6,6 +6,7 @@ import {ReloadIcon} from '@radix-ui/react-icons';
 import {useFormStatus} from 'react-dom';
 import {SignInButton} from "@clerk/nextjs";
 import {FaHeart, FaRegHeart} from 'react-icons/fa';
+import {LuPen, LuTrash2} from "react-icons/lu";
 
 type  btnSize = 'default' | 'lg' | 'sm';
 type SubmitButtonProps = {
@@ -61,4 +62,28 @@ export const CardSubmitButton = ({isFavorite}:{isFavorite:boolean}) => {
              )}
          </Button>
      )
+}
+
+
+type actionType = "edit" | "delete";
+
+
+export const IconButton = ({actionType}:{actionType:actionType}) => {
+    const {pending} = useFormStatus()
+
+    const renderIcon =() => {
+        switch (actionType) {
+            case "edit":
+                return <LuPen/>
+            case "delete":
+                return <LuTrash2/>;
+            default:
+                const never:never = actionType;
+                throw new Error(`Invalid action type:${never}`);
+        }
+    }
+
+    return <Button type="submit" size="icon" variant="link" className="p-2 cursor-pointer">
+        {pending ? <ReloadIcon className="animate-spin"/> : renderIcon() }
+    </Button>
 }
