9505c51d0f926c4a54b7e830ef240a7677ec8c84
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-10 20:18:10 +0300

    createed laoding.tsx for the rental page

---
+++ src/app/rentals/loading.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import LoadingTable from "@/components/booking/LoadingTable";
+
+function loading() {
+    return (
+<LoadingTable/>
+    );
+}
+
+export default loading;
