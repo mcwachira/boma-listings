711b132819a146ec350788b30ac731c6e47273df
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-05-15 02:26:56 +0300

    Created the admin page and added some charts to show the number of users and booking data

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,20 @@
+import {ChartsLoadingContainer, StatsLoadingContainer} from '@/components/admin/Loading';
+import React, { Suspense } from 'react';
+import StatsContainer from "@/components/admin/StatsContainer";
+import ChartsContainer from '@/components/admin/ChartsContainer';
+
+function AdminPage() {
+    return (
+        <>
+        <Suspense fallback={<StatsLoadingContainer/>}>
+            <StatsContainer/>
+        </Suspense>
+
+            <Suspense fallback={<ChartsLoadingContainer/>}>
+                <ChartsContainer/>
+            </Suspense>
+        </>
+    );
+}
+
+export default AdminPage;
